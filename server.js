a27cf7b27375a7993dbfcdd4643c86e1e3dc3789
Apple-Acc <srikanthvln98@gmail.com>
2022-12-26 13:08:13 +0530

    Enabling CORS as well

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const filesaver = require('fs')
+const cors = require('cors')
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors())
 
 app.get('/getAllProjects',(req,res)=> {
     const rawFile = filesaver.readFileSync('db.json')
